60906638fd298d945653fe96d24c374a9e67a2b1
jonBarcus <barcus.j@gmail.com>
2017-07-01 17:06:58 -0400

    completed step 6

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+
+  before_filter :configure_sign_up_params, only: [:create]
   # GET /resource/sign_up
   # def new
   #   super
@@ -39,9 +41,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.for(:sign_up) << :soft_token
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
